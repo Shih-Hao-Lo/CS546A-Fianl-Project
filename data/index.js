ccb68c312c1515f97202b6f3d100a0c3416fd2a6
SHIH-HAO LO <slo@stevens.edu>
2019-04-02 23:02:34 -0400

    update data/index

--- data/index.js
+++ data/index.js
@@ -2,10 +2,14 @@ const patientData = require("./patient");
 const doctorData = require("./doctor");
 const roomData = require('./room');
 const medicineData = require('./medicine');
+const reservationData = require('./reservation');
+const prescriptionData = require('./prescription');
 
 module.exports = {
     patient: patientData,
     doctor: doctorData,
     room: roomData,
-    medicine: medicineData
+    medicine: medicineData,
+    reservation: reservationData,
+    prescription: prescriptionData
 };
