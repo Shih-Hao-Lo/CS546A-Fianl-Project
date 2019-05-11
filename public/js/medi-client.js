6f4f7cedd063ad87703b624a9ef17421f32b402e
SIT-Ping <hlin17@stevens.edu>
2019-05-11 18:52:36 -0400

    Set "min" attr. to date (user can't pick the date before current date)

--- public/js/medi-client.js
+++ public/js/medi-client.js
@@ -194,3 +194,19 @@ function backToDashboard() {
 function changePWD() {
   location.href = '/change-password';
 }
+
+function DateRestrict() {
+    let today = new Date();
+    let day = today.getDate();
+    let month = today.getMonth() + 1;
+    let year = today.getFullYear();
+    if (day < 10) {
+        day = '0' + day
+    }
+    if (month < 10) {
+        month = '0' + month
+    }
+
+    today = year + '-' + month + '-' + day;
+    document.getElementById("resvDate").setAttribute("min", today);
+}
